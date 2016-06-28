148f108b7d96ac491374be772b57e48b7b0d19e2
Victor Martinez <studioquetzal@gmail.com>
2016-06-28 00:37:17 -0500

    Hola mundo

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Practica PHP</title>
+  </head>
+  <body>
+    <h1>Practicas del libro PHP para Dummies</h1>
+
+    <?php
+      echo "<p>Hola mundo</p>";
+     ?>
+
+  </body>
+</html>
