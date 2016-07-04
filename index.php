4c0ff83d25604e3a36cea9c6e8b2642a9eae379e
Victor Martinez <studioquetzal@gmail.com>
2016-07-04 00:52:07 -0500

    Agregados Scripts para BD
    Se agregaron los script para la creacion de la bd

--- index.php
+++ index.php
@@ -3,13 +3,32 @@
   <head>
     <meta charset="utf-8">
     <title>Practica PHP</title>
+    <script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   </head>
   <body>
-    <h1>Practicas del libro PHP para Dummies</h1>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <a class="navbar-brand">Tienda de Mascotas</a>
+          <ul class="nav navbar-nav">
+            <li class="active">
+              <a href="index.php">Home</a>
+            </li>
+            <li>
+              <a href="catalogo.php">Catalogo Mascotas</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
-    <?php
-      echo "<p>Hola mundo</p>";
-     ?>
+      <img src="https://static.pexels.com/photos/69372/pexels-photo-69372.jpeg" class="img-responsive" alt="Image">  
 
   </body>
 </html>
